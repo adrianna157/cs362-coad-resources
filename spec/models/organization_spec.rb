29e143bc588575925c04e0086acaad1d1f76ed8c
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-04-16 10:05:25 -0700

    Organization relationship test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+    describe 'relationship' do
+        it 'should have many tickets and users' do
+            organization = Organization.new
+            expect(organization).to respond_to(:tickets, :users)
+        end
+    end
 
 end
