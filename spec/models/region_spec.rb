9eafb5d539f619059b0dd58216e9da83cde99249
Yong Bakos <ybakos@humanoriented.com>
2020-04-21 09:31:27 -0700

    Region: Test validations, extract into let.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,28 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  let(:region) { Region.new(name: 'FAKE') }
+
   describe "attributes" do
     it "has a name" do
-      region = Region.new
       expect(region).to respond_to(:name)
     end
   end
 
   describe "relationships" do
     it "has many tickets" do
-      region = Region.new
       expect(region).to have_many(:tickets)
     end
   end
 
-  # Validations
+  describe "validations" do
+    it "validates name" do
+      expect(region).to validate_presence_of(:name)
+    end
+    it "validates length of name" do
+      expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+    end
+    it "validates uniqueness of name" do
+      expect(region).to validate_uniqueness_of(:name).case_insensitive
+    end
+  end
 
   describe "#to_s" do
     it "has a string representation that is the name" do
-      expected_name = 'FAKE'
-      region = Region.new(name: expected_name)
-      expect(region.to_s).to eq(expected_name)
+      expect(region.to_s).to eq('FAKE')
     end
+  end
+
+  describe "::unspecified" do
+    # TODO? HMMMM..
   end
 
 end
