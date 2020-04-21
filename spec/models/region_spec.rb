466bd9237388aaf998e42f7866f7368e9480b790
Yong Bakos <ybakos@humanoriented.com>
2020-04-21 08:43:36 -0700

    Region: test existence.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  it "exists" do
+    Region.new
+  end
+
+  # Properties / Attributes
+
+  # Relationship
+
+  # Validations
+
+  # Methods
+
 end
