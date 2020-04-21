5f5923e7a87df669d54a65e17c651221ef6c7d45
Yong Bakos <ybakos@humanoriented.com>
2020-04-21 08:49:08 -0700

    Region: Test presence of name.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-  it "exists" do
-    Region.new
+  describe "attributes" do
+    it "has a name" do
+      region = Region.new
+      expect(region).to respond_to(:name)
+    end
   end
+
 
   # Properties / Attributes
 
