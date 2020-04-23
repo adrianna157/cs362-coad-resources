662f586e68fc63c982e58d3085d9c94f2a3cc2cb
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-04-22 19:00:48 -0700

    create unit test for name attribute References #2

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
+   describe "name"do 
+    it "has a name" do
+        region = Region.new
+        expect(region).to respond_to(:name)
+    end
+end
 
 end
