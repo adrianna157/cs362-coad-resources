847049a6345b4534d18cd3c93d9e872b617c36d2
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-04-27 21:13:58 -0700

    create and pass relationships for tickets References #4

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  describe "relationships" do
+    it "belongs to" do
+        ticket = Ticket.new
+        should {belong_to(:region)}
+        should {belong_to(:resource_category)}
+        should {belong_to(:organization).optional}
+        
+            
+     end
+   end    
 end
