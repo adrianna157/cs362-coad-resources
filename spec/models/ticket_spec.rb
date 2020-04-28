ee6bff604c1444ab50c633ac802c0aedfe14fe9a
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-04-27 21:28:10 -0700

    create/pass validate_presence_of References #4

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -8,8 +8,24 @@ RSpec.describe Ticket, type: :model do
         should {belong_to(:region)}
         should {belong_to(:resource_category)}
         should {belong_to(:organization).optional}
-        
-            
      end
-   end    
+   end
+   describe "validations" do
+    it "validates presence of name" do
+        ticket = Ticket.new
+            expect(ticket).to validate_presence_of( :name )
+       end
+    it "validates presence of phone" do
+        ticket = Ticket.new
+            expect(ticket).to validate_presence_of( :phone )
+       end
+    it "validates presence of region id" do
+        ticket = Ticket.new
+            expect(ticket).to validate_presence_of( :region_id )
+       end
+    it "validates presence of resource category" do
+        ticket = Ticket.new
+            expect(ticket).to validate_presence_of( :resource_category_id )
+       end      
+   end      
 end
