b799f0f633072922e4a2bf669f1d05a83077df17
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-04-30 11:27:29 -0700

    ResourceCategory validations test length of name References #3

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -23,6 +23,9 @@ RSpec.describe ResourceCategory, type: :model do
         it "validates presences of name" do
             expect(resource_category).to validate_presence_of(:name)
         end
+        it "validates length of name" do
+            expect(resource_category).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+        end
     end
 
 end
