33ef23aaa4c40bf7f15ec323a3831815143dc718
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-04-30 10:59:26 -0700

    User attributes test email References #5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+    let (:user) { User.new() }
+
+    describe 'attributes' do
+        it 'has an email' do
+            expect(user).to respond_to(:email)
+        end
+    end
+
+    describe 'relationship' do
+        it 'belongs to an organization' do
+            expect(user).to belong_to(:organization)
+        end
+    end
+
+    describe 'method' do
+        it 'has a string that is a name' do
+            expected_email = 'fake@example.com'
+            user = User.new(email: expected_email)
+            expect(user.to_s).to eq(expected_email)
+        end
+    end 
 end
