38450b6ad1c8bf7c6cd5924eccca1ad9c25b0d62
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-04-30 12:04:45 -0700

    Ticket test removed duplication and put let block up top References #4

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,63 +1,53 @@
 
 require 'rails_helper'
 
-
 RSpec.describe Ticket, type: :model do
 
   let(:closed_ticket){create(:closed_ticket)}
   let(:open_ticket){create(:ticket)} 
+  let (:ticket) { Ticket.new() }
 
 
-  describe "relationships" do
-    it "belongs to" do
-        ticket = Ticket.new
-        should {belong_to(:region)}
-        should {belong_to(:resource_category)}
-        should {belong_to(:organization).optional}
-     end
-   end
-   describe "validations" do
-    it "validates presence of name" do
-        ticket = Ticket.new
+
+    describe "relationships" do
+        it "belongs to" do
+            should {belong_to(:region)}
+            should {belong_to(:resource_category)}
+            should {belong_to(:organization).optional}
+        end
+    end
+
+    describe "validations" do
+        it "validates presence of name" do
             expect(ticket).to validate_presence_of( :name )
-       end
-    it "validates presence of phone" do
-        ticket = Ticket.new
+        end
+        it "validates presence of phone" do
             expect(ticket).to validate_presence_of( :phone )
-       end
-    it "validates presence of region id" do
-        ticket = Ticket.new
+        end
+        it "validates presence of region id" do
             expect(ticket).to validate_presence_of( :region_id )
-       end
-    it "validates presence of resource category" do
-        ticket = Ticket.new
+        end
+        it "validates presence of resource category" do
             expect(ticket).to validate_presence_of( :resource_category_id )
-       end
-    it "validates presences the length of a name" do
-            ticket = Ticket.new
+        end
+        it "validates presences the length of a name" do
             expect(ticket).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
         end
-    it "validates presences the length of a name" do
-            ticket = Ticket.new
+        it "validates presences the length of a name" do
             expect(ticket).to validate_length_of(:description).is_at_most(1020).on(:create)
-        end
-    # it "validates" do
-    #         ticket = Ticket.new
-    #         expect(ticket).to validates(:phone).
-    #     end
-    it "has attributes" do
-        ticket = Ticket.new
-        expect(ticket).to respond_to(:name, :description, :phone, :organization_id, :created_at,
-        :updated_at, :closed_at, :closed, :closed_at, :resource_category_id, :region_id)
+        end 
     end
-end
-   
-    describe "#open" do
-        it "retrieves only non-closed tickets without an organization" do
-            open_tickets = Ticket.open
-            expect(open_tickets).to include(open_ticket)
-            # expect(open)tickets.not_to include(closed_ticket)
+
+    describe 'methods' do
+    
+        describe "#open" do
+            it "retrieves only non-closed tickets without an organization" do
+                open_tickets = Ticket.open
+                expect(open_tickets).to include(open_ticket)
+                # expect(open)tickets.not_to include(closed_ticket)
+            end
         end
     end
-   end      
+
+end      
 
