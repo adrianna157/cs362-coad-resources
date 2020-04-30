63fb1fd1be881113f231bf7656beb6723ce902ff
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-04-30 11:22:29 -0700

    ResourceCategory attribute test name References #3

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+    let (:resource_category) { ResourceCategory.new() }
+
+    describe 'attributes' do
+        it 'has a name' do
+            expect(resource_category).to respond_to(:name)
+        end
+    end
 end
