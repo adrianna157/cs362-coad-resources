f3ea2c4236f193cb1eac7e5ea2d6132802ee06ad
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-04-30 11:00:09 -0700

    User relationship test organization References #5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,18 +9,9 @@ RSpec.describe User, type: :model do
             expect(user).to respond_to(:email)
         end
     end
-
     describe 'relationship' do
         it 'belongs to an organization' do
             expect(user).to belong_to(:organization)
         end
     end
-
-    describe 'method' do
-        it 'has a string that is a name' do
-            expected_email = 'fake@example.com'
-            user = User.new(email: expected_email)
-            expect(user.to_s).to eq(expected_email)
-        end
-    end 
 end
