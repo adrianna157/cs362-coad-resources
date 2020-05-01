344c51a31c9b78ab0fc5e15936a0a4efa29edc75
Yong Bakos <ybakos@humanoriented.com>
2020-05-01 14:46:50 -0700

    User: Introduce factory and verify.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  let(:normal_user) { build(:organization_member) }
+  let(:admin) { build(:admin) }
+
+  describe 'an example of verifying your factory is sound' do
+    it 'is ok when generated from our factory' do
+      first_user = create(:user)
+      expect(first_user).to be_valid
+      expect(first_user).to be_persisted
+      second_user = create(:user)
+      expect(second_user).to be_valid
+      expect(second_user).to be_persisted
+    end
+  end
+
 end
