93017bf20698793f085514248594af02c5ab7910
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-05-05 09:25:23 -0700

    create users factory and regions_controller test

---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+    factory :user do
+        email {'testertesterson@gmail.com'}
+        password {'fake_password'}
+
+        trait :admin do
+            role{:admin}
+        end
+
+        trait :organization_member do
+            role{:organization}
+        end
+
+        after :create do |user|
+            user.confirm
+        end
+    end
+end
