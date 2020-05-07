153c82be48064c4f814d58f191eea71de18b6838
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-05-07 12:30:17 -0700

    user_spec and user factory spacing issues

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,23 +1,20 @@
 FactoryBot.define do
   
-  factory :user do
-<<<<<<< HEAD
-      email
-=======
-      email 
->>>>>>> 71b73e3f1ed8809fb429f564795d1b084e960ab6
-      password {'fake_password'}
+    factory :user do
+        email
+        password {'fake_password'}
 
-      trait :admin do
-          role{:admin}
-      end
+        trait :admin do
+            role{:admin}
+        end
 
-      trait :organization_member do
-          role{:organization}
-      end
+        trait :organization_member do
+            role{:organization}
+        end
 
-      after :create do |user|
-          user.confirm
-      end
-  end
+        after :create do |user|
+            user.confirm
+        end
+    end
+    
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,39 +6,39 @@ RSpec.describe User, type: :model do
     let(:admin) { build(:admin) }
     let (:user) { User.new() }
 
-    # describe 'attributes' do
-    #     it 'has an email' do
-    #         expect(user).to respond_to(:email)
-    #     end
-    # end
+    describe 'attributes' do
+        it 'has an email' do
+            expect(user).to respond_to(:email)
+        end
+    end
     
-    # describe 'relationship' do
-    #     it 'belongs to an organization' do
-    #         expect(user).to belong_to(:organization)
-    #     end
-    # end
+    describe 'relationship' do
+        it 'belongs to an organization' do
+            expect(user).to belong_to(:organization)
+        end
+    end
 
-    # describe 'validation' do
-    #     it 'validates presence of email' do
-    #         expect(user).to validate_presence_of(:email)
-    #     end 
-    #     it "validates length of email" do
-    #         expect(user).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
-    #     end
-    #     it 'validates format of email' do
-    #         expect(user).to allow_value('valid@example.com').for(:email)
-    #         expect(user).to_not allow_value('invalid.com', '@invalid.com', 'INVALID', 'invalid@fake').for(:email)
-    #     end 
-    #     it "validates uniqueness of email" do
-    #         expect(user).to validate_uniqueness_of(:email).case_insensitive
-    #     end
-    #     it 'validates presence of password' do
-    #         expect(user).to validate_presence_of(:password).on(:create)
-    #     end 
-    #     it "validates lenght of password" do
-    #         expect(user).to validate_length_of(:password).is_at_least(7).is_at_most(255).on(:create)
-    #     end
-    # end
+    describe 'validation' do
+        it 'validates presence of email' do
+            expect(user).to validate_presence_of(:email)
+        end 
+        it "validates length of email" do
+            expect(user).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
+        end
+        it 'validates format of email' do
+            expect(user).to allow_value('valid@example.com').for(:email)
+            expect(user).to_not allow_value('invalid.com', '@invalid.com', 'INVALID', 'invalid@fake').for(:email)
+        end 
+        it "validates uniqueness of email" do
+            expect(user).to validate_uniqueness_of(:email).case_insensitive
+        end
+        it 'validates presence of password' do
+            expect(user).to validate_presence_of(:password).on(:create)
+        end 
+        it "validates lenght of password" do
+            expect(user).to validate_length_of(:password).is_at_least(7).is_at_most(255).on(:create)
+        end
+    end
 
     describe 'method' do
         describe "#to_s" do
