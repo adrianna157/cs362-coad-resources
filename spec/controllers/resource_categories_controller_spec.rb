668f3ba64c7a695be13ef0de63b67239e33da0e0
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-05-07 12:45:49 -0700

    resource category controller public user test passed

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
 
-    # context 'As a public user' do
-    #     describe '/tickets/new' do
-    #         specify { expect(get(:index)).to be_successful }
-    #     end
-    # end
+    context 'As a public user' do
+        describe '/resource_categories' do
+            specify { expect(get(:index)).to redirect_to(new_user_session_path) }
+        end
+    end
+
+
 
 end
