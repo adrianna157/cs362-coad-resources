0dea628e797475c3fdca118a1cb2b2a06448e5ed
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-05-07 12:58:22 -0700

    passed test resource_categories_controller

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -8,6 +8,24 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         end
     end
 
+    context 'As an organization user' do
+        let(:user) {create(:user)}
+        before(:each) {sign_in(user)}
+
+        describe 'GET #index' do
+            specify{expect(get(:index)).to redirect_to(dashboard_path)}
+        end
+    end
+
+    context 'As an admin' do
+        let(:admin_user) {create(:user, :admin)}
+        before(:each) {sign_in(admin_user)}
+        
+        describe 'GET #index' do
+            specify{expect(get(:index)).to be_successful}
+        end
+    end
+
 
 
 end
