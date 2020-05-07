a5cf97f8aee71806b2cf2e4ea05d0559d331e859
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-05-07 09:41:33 -0700

    fixed syntax error in ticket_spec.rb

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
 
+    context 'As a public user' do
+        describe '/tickets/new' do
+            specify { expect(get(:index)).to be_successful }
+        end
+    end
+
 end
