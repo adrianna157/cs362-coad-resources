b1fcb467874ce59edbf026cf2e12f6e90155c515
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-05-07 09:24:09 -0700

    tickets controller spec admin user tests

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -17,13 +17,13 @@ RSpec.describe TicketsController, type: :controller do
         end
     end
 
-    # context 'As an admin' do
-    #     let(:admin_user) {create(:user, :admin)}
-    #     before(:each) {sign_in(admin_user)}
+    context 'As an admin' do
+        let(:admin_user) {create(:user, :admin)}
+        before(:each) {sign_in(admin_user)}
         
-    #     describe 'GET #index' do
-    #         specify{expect(get(:index)).to be_successful}
-    #     end
-    # end
+        describe 'GET #new' do
+            specify{ expect(get(:new)).to be_successful }
+        end
+    end
 
 end
