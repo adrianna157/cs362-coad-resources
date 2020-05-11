c13a8e253f2995193333ec7b681d81e8d231df66
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-05-11 10:15:55 -0700

    dashboard controller spec implemented

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,5 +2,26 @@ require 'rails_helper'
 #this is a test  commit for this file for the 3rd time
 
 RSpec.describe DashboardController, type: :controller do
+    context 'As a public user' do
+        describe '/dashboard' do
+            specify { expect(get(:index)).to redirect_to(new_user_session_path)}
+        end
+    end
 
+    context 'As an organization user' do
+        let(:user) {create(:user)}
+        before(:each) {sign_in(user)}
+
+        describe 'GET #index' do
+        end
+    end
+
+    context 'As an admin' do
+        let(:admin_user) {create(:user, :admin)}
+        before(:each) {sign_in(admin_user)}
+        
+        describe 'GET #index' do
+            specify{expect(get(:index)).to be_successful}
+        end
+    end
 end
