ceaf5f4a943640c6cd70a17aff5ca437b3e9795e
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-05-11 10:19:58 -0700

    create test for organizations controller and pass all three tests

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
+#this is a test  commit for this file for the 3rd time
 
 RSpec.describe OrganizationsController, type: :controller do
+    context 'As a public user' do
+        describe '/organizations' do
+            specify { expect(get(:index)).to redirect_to(new_user_session_path)}
+        end
+    end
 
+    context 'As an organization user' do
+        let(:user) {create(:user)}
+        before(:each) {sign_in(user)}
+
+        describe 'GET #index' do
+            specify{expect(get(:index)).to be_successful}
+        end
+    end
+
+    context 'As an admin' do
+        let(:admin_user) {create(:user, :admin)}
+        before(:each) {sign_in(admin_user)}
+        
+        describe 'GET #index' do
+            specify{expect(get(:index)).to be_successful}
+        end
+    end
 end
