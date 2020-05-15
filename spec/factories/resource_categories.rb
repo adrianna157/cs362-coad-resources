88475bbbbb043e110d0fe170ff8cc3857c8dbf04
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-05-14 21:08:20 -0700

    create/pass scope for active resource category test

--- spec/factories/resource_categories.rb
+++ spec/factories/resource_categories.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
     factory :resource_category do
         name {'THE NAME'+ (rand(1..1000).to_s)}
         organization_ids{create(:organization).id}
+
+        trait :active do
+        active{true}
+        end
+        trait :inactive do
+            active{false}
+        end
     end
 end
