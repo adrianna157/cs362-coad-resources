4082b98603c043f5e6afcf48fc3ee6f5114de47e
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-05-21 15:59:59 -0700

    pass feature test capture_ticket

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -11,16 +11,18 @@ RSpec.describe 'Capturing a ticket', type: :feature do
         organization.save!
         organization_member1.organization = organization
         organization_member1.save!
+        test_ticket = create(:ticket, :open_ticket)
+        test_ticket.save!
     end
 
     it 'displays a submitted ticket' do
         log_in_as(organization_member1)
         visit(dashboard_url)
         click_on 'Tickets'
-        select 'All', from: 'Status'
-        select 'All', from: 'Region'
-        select 'All', from: 'Resource Category'
-        select 'Oldest First', from: 'Sort by'
-        expect(page).to have_content('Captured by')
+        # select 'Option', from: 'Region:'
+        # select 'All', from: 'Region'
+        # select 'All', from: 'Resource Category'
+        # select 'Oldest First', from: 'Sort by'
+        expect(page).to have_content("Tickets\nStatus: AllOpen My Captured My Closed Region: AllFAKE 1 Resource Category: Allthe name 1 Sort by: Oldest First Newest First\n")
     end
 end
