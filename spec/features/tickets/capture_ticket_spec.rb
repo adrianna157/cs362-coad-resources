04aa62d32bad7b6c9a2965870611f275d4afcf94
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-05-21 13:28:15 -0700

    create capture ticket feature test

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
+require '/Users/adriannaguevarra/Desktop/Projects/SoftwareEngineeringII/cs362-coad-resources/spec/support/user_helper.rb'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
-
+    
+    let (:organization_member1) {create(:user, :organization_member)}
+   
+    it 'displays a submitted ticket' do
+        log_in_as(organization_member1)
+        visit(dashboard_url)
+       expect(page).to have_field 'tab'
+    end
 end
