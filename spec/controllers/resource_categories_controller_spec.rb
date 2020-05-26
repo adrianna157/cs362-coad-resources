e49df468e1129729a9c896366178ddd97b2960f4
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-05-26 16:40:47 -0700

    finished context PUT Update on resource_categories_controller_spec

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
     context 'As an admin' do
         let(:admin_user) {create(:user, :admin)}
-        let(:resource_categories_admin) {create(:resource_category)}
+        let(:resource_category) {create(:resource_category)}
         before(:each) {sign_in(admin_user)}
         
         describe 'GET #index' do
@@ -28,25 +28,34 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
          describe 'GET #show' do
             it "gets show successfully" do
-                expect(get(:show, params: { id: resource_categories_admin.id })).to be_successful
+                expect(get(:show, params: { id: resource_category.id })).to be_successful
             end
         end
 
         describe 'GET #new' do
             it "gets new successfully" do
-                expect(get(:new, params: { id: resource_categories_admin.id })).to be_successful
+                expect(get(:new, params: { id: resource_category.id })).to be_successful
             end
         end
 
         describe 'POST #create' do
             it "gets show successfully" do
-                expect(post(:create, params: { id: resource_categories_admin.id, resource_category: attributes_for(:resource_category) })).to redirect_to(resource_categories_path)
+                expect(post(:create, params: { id: resource_category.id, resource_category: attributes_for(:resource_category) })).to redirect_to(resource_categories_path)
             end
         end
 
          describe 'GET #edit' do
             it "gets edit successfully" do
-                expect(get(:edit, params: { id: resource_categories_admin.id })).to be_successful
+                expect(get(:edit, params: { id: resource_category.id })).to be_successful
+            end
+        end
+
+             describe 'PUT #update' do
+             
+            it "puts update successfully" do
+                
+                expect(get(:update, params: { id: resource_category.id, resource_category: attributes_for(:resource_category)})).to redirect_to(resource_category_path(resource_category))
+            
             end
         end
 
