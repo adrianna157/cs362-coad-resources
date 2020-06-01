5f8196f26dd84a42cfa115ecd41902e8ee04e02f
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-06-01 12:12:08 -0700

    create_ticket_spec get help option displayed

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Updating an Organization', type: :feature do
 
     # let(:organization) {create(:organization)}
+    # let(:admin) {create(:user, :admin)}
 
     # context 'As an admin' do 
     #     it 'updating an organization succesfully' do
+    #         log_in_as(admin)
     #         visit edit_organization_path(organization)
-    #         fill_in(:name).with('New Fake Name')
+    #         fill_in('Applicant Name', :with => 'New Fake Name')
     #         click 'Save'
     #         expect(page).to have_content('New Fake Name')
     #     end
--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
 
+    let(:user) {create(:user)} 
+
+    it 'Get Help option is displayed' do
+        visit('/')
+        click_on 'Get Help'
+        # expect(page).to redirect_to(new_ticket)
+    end
+
 end
