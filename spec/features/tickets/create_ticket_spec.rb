ab185d4e103d09059a6bdea2209e84f40402aca9
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-06-01 12:19:15 -0700

    create_ticket_spec success method is sent

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Creating a Ticket', type: :feature do
 
     let(:user) {create(:user)} 
+    let(:region) {create(:region)}
 
     it 'Get Help option is displayed' do
-        visit('/')
+        visit root_path
         click_on 'Get Help'
-        # expect(page).to redirect_to(new_ticket)
+        # expect(page).to redirect_to(tickets_url)
+        fill_in 'Full Name', with: 'Fake Name'
+        fill_in 'Phone Number', with: '1231234567'
+        select @region.unspecified, from: 'Region'
     end
 
 end
