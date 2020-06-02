710522c2d4d9835621931f55a3f39561ee483bdc
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-06-01 18:29:49 -0700

    create_ticket_spec displays an unsuccessful ticket

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe 'Creating a Ticket', type: :feature do
     it 'Displays a successfull ticket submission' do
         visit root_path
         click_on 'Get Help'
-        # expect(page).to redirect_to(tickets_url)
         fill_in 'Full Name', with: 'Fake Name'
         fill_in 'Phone Number', with: '3036531234'
         select @region.name, from: 'Region'
@@ -20,7 +19,18 @@ RSpec.describe 'Creating a Ticket', type: :feature do
         fill_in 'Description', with: 'Fake'
         click_on 'Send this help request'
         expect(page).to have_content('Ticket Submitted')
+    end
 
+    it 'Displays an unsuccessfull ticket submission' do
+        visit root_path
+        click_on 'Get Help'
+        fill_in 'Full Name', with: 'Fake Name'
+        fill_in 'Phone Number', with: 'INVALID'
+        select @region.name, from: 'Region'
+        select @resource_category.name, from: 'Resource Category'
+        fill_in 'Description', with: 'Fake'
+        click_on 'Send this help request'
+        expect(page).to have_content('prohibited')
     end
 
 end
