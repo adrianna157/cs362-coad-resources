7e8938ae4d633a427e0f6248c4f2ec7b27e8d383
Adrianna Guevarra <guevarra@oregonstate.edu>
2020-06-02 06:42:11 -0700

    admin_deletes_ticket feature test created and passed

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -2,13 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
     
-    let (:ticket) {create(:ticket)}
-    let (:admin_member) {create(:user, :admin)}
-
-    it 'Displays a success screen for deleted ticket' do
-        log_in_as(admin_member)
-        visit dashboard_path
-        # expect(page).to have_content('FAKE NAME')
-        # click_link ticket.name
+   let (:admin_member1) {create(:user, :admin)}
+   
+    before do
+        organization = create(:organization)
+        organization.status = 'approved'
+        organization.save!
+        admin_member1.organization = organization
+        admin_member1.save!
+        test_ticket = create(:ticket, :open_ticket)
+        test_ticket.save!
+    end
+    it 'releases a ticket ' do
+        log_in_as(admin_member1)
+        visit dashboard_url
+        click_on 'FAKE NAME'
+        click_on 'Delete'
+        expect(page).to have_content("Ticket 1 was deleted")
+        expect(page).not_to be_nil
     end
 end
